fe9024a78afaf39b419b8e7802d4ca93f7c7e79f
gshah214 <shahgauri214@gmail.com>
2022-03-12 16:45:20 -0500

    more code updates

--- app.js
+++ app.js
@@ -10,71 +10,66 @@ var app = express();
 var server = http.createServer(app);
 
 const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100 // limit each IP to 100 requests per windowMs
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100 // limit each IP to 100 requests per windowMs
 });
 
-server.listen(3000, function () {
-    console.log(`Server listening to port 3000`);
+server.listen(3000,function(){ 
+    console.log("Server listening on port: 3000");
 })
+var db = new sqlite3.Database('./database/clients.db');
 
-var db = new sqlite3.Database('./database/client.db');
 
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
-app.use(express.static(path.join(__dirname, './public')));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname,'./public')));
 app.use(helmet());
 app.use(limiter);
 
-db.run('CREATE TABLE IF NOT EXISTS client(name TEXT, phone TEXT, email TEXT, sqfoot TEXT, bedrooms TEXT, bathrooms, TEXT)');
+db.run('CREATE TABLE IF NOT EXISTS client(name TEXT, phone TEXT, email TEXT, squarefootage TEXT, build TEXT, bedrooms TEXT, bathrooms TEXT)');
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
+app.get('/', function(req,res){
+    res.sendFile(path.join(__dirname,'./public/index.html'));
+  });
 
-// Insert
-app.post('/add', function (req, res) {
-    db.serialize(() => {
-        db.run('INSERT INTO client(name,phone,email,sqfoot,bedrooms,bathrooms) VALUES(?,?,?,?,?,?)', [req.body.name, req.body.phone, req.body.email, req.body.sqfoot, req.body.bedrooms, req.body.bathrooms], function (err) {
-            if (err) {
-                return console.log(err.message);
-            }
-            console.log("New client has been added");
-            res.send("New client has been added into the database with Name = " + req.body.name +
-                " , Phone = " + req.body.phone +
-                " , Email = " + req.body.email +
-                " , Sq. Footage = " + req.body.sqfoot +
-                " , Bedrooms = " + req.body.bedrooms +
-                " , Bathrooms = " + req.body.bathrooms);
-        });
-    });
-});
+  // Insert
+app.post('/add', function(req,res){
+    db.serialize(()=>{
+      db.run('INSERT INTO client(name, phone, email, squarefootage, build, bedroom, bathroom) VALUES(?,?,?,?,?,?,?)', [req.body.name, req.body.phone, req.body.email, req.body.squarefootage, req.body.build, req.body.bedrooms, req.body.bathrooms], function(err) {
+        if (err) {
+          return console.log(err.message);
+        }
+        console.log("New client has been added");
+        res.send("New client has been added into the database with name = "+req.body.name+ ", number = "+req.body.number+ ", email = "+req.body.email+", squarefootage = "+req.body.squarefootage+", build = "+req.body.build+", bedrooms = "+req.body.bedroom+", and bathrooms "+req.body.bathroom+"");
+      });
+  });
+  });
+
 
 // View
-app.post('/view', function (req, res) {
-    db.serialize(() => {
-        db.each('SELECT name NAME, phone PHONE FROM client WHERE name =?', [req.body.name], function (err, row) { //db.each() is only one which is funtioning while reading data from the DB
-            if (err) {
-                res.send("Error encountered while displaying");
-                return console.error(err.message);
-            }
-            res.send(` NAME: ${row.NAME},    PHONE: ${row.PHONE}`);
-            console.log("Entry displayed successfully");
-        });
+app.post('/view', function(req,res){
+    db.serialize(()=>{
+      db.each('SELECT name NAME, phone PHONE, email EMAIL FROM client WHERE name =?', [req.body.name], function(err,row){     //db.each() is only one which is funtioning while reading data from the DB
+        if(err){
+          res.send("Error encountered while displaying");
+          return console.error(err.message);
+        }
+        res.send(` Name: ${row.NAME},    Phone: ${row.PHONE}, Email: ${row.EMAIL}`);
+        console.log("Entry displayed successfully");
+      });
     });
-});
+  });
+
 
 //UPDATE
-app.post('/update', function (req, res) {
-    db.serialize(() => {
-        db.run('UPDATE client SET name=?, phone=?, email=?, sqfoot=?, bedrooms=?, bathrooms=? WHERE name=?', [req.body.name, req.body.phone, req.body.email, req.body.sqfoot, req.body.bedrooms, req.body.bathrooms], function (err) {
-            if (err) {
-                res.send("Error encountered while updating");
-                return console.error(err.message);
-            }
-            res.send("Entry updated successfully");
-            console.log("Entry updated successfully");
-        });
+app.post('/update', function(req,res){
+    db.serialize(()=>{
+      db.run('UPDATE emp SET name = ? phone = ? email = ? squarefootage = ? build = ? bedrooms = ? bathrooms = ? WHERE name = ? ', [req.body.name,req.body.phone,req.body.email,req.body.squarefootage,req.body.build,req.body.bedrooms,req.body.bathrooms], function(err){
+        if(err){
+          res.send("Error encountered while updating");
+          return console.error(err.message);
+        }
+        res.send("Entry updated successfully");
+        console.log("Entry updated successfully");
+      });
     });
-});
+  });
